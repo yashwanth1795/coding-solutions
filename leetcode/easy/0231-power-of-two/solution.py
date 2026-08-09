4fefb04f7abe4a32514236b83d839616074a68ec
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-09 18:40:05 +0530

    ✅ Power of Two | Easy | Python | Runtime: 4 ms (5.27%) | Memory: 19.1 MB | 2026-08-09

--- leetcode/easy/0231-power-of-two/solution.py
+++ leetcode/easy/0231-power-of-two/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n<0:
+        if n<=0:
             return False
-        else:
-            return (n &(n-1)==0)
+
+        return (n &(n-1)==0)
 
         
