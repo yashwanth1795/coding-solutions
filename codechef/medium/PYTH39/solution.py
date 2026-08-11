afd635ede78a85b82232c81c30fe90361d5c284d
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-11 22:20:42 +0530

    ✅ PYTH39 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/PYTH39/solution.py
@@ -0,0 +1,7 @@
+# We need to output 2569 using string concatenation.
+# The code below is incorrect - Debug the code to solve the problem
+
+string1 = "25"
+string2 = "69"
+
+print(string1 + string2)
