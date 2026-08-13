edb5dc88323e08da52f97ce5f696c22cb5ec9d99
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-13 20:09:02 +0530

    ✅ QCROZK02 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-13

--- codechef/medium/QCROZK02/solution.py
+++ codechef/medium/QCROZK02/solution.py
@@ -1,16 +1,27 @@
 import numpy as np
 
-def even_number_mapper(numbers):
-    # Convert the input list to a NumPy array
-    arr = np.array(numbers)
+def convert_to_grades(scores):
+    # Create a NumPy array from the input list
+    score_array = np.array(scores)
     
-    even_mask=arr%2==0
-    return even_mask
+    # Create an empty string array of the same shape as score_array
+    grade_array = np.empty_like(score_array, dtype='U1')
+    grade_array[score_array>=90]='A'
+    grade_array[(score_array>=80) & (score_array<=89)]='B'
+    grade_array[(score_array>=70) & (score_array<=79)]='C'
+    grade_array[(score_array>=60) & (score_array<=69)]='D'
+    grade_array[score_array<60]='F'
+    
 
-# Get input from the user
-input_numbers = input().split()
-numbers_list = [int(num) for num in input_numbers]
+    
+    return grade_array
+
+# Get input from user
+scores = input().split()
+scores = [int(score) for score in scores]
+
+# we can also use map to take input: scores = list(map(int, input().split()))
 
 # Call the function and print the result
-result = even_number_mapper(numbers_list)
+result = convert_to_grades(scores)
 print(result)
