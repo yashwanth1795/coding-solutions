95d8c0d270d2316a70f08a54953975217a80d399
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-13 15:02:22 +0530

    ✅ PYTH73 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-13

--- codechef/medium/PYTH73/solution.py
+++ codechef/medium/PYTH73/solution.py
@@ -2,7 +2,7 @@ score = int(input())
 
 if (score==100):
     print("Perfect score")
-elif (80>=100):
+elif (score>=80 and score<=100):
     print("Almost perfect score")
 else:
     print("Nice try")
