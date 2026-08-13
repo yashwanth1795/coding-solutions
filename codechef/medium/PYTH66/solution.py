8a28dc0f7759c42f91eaf422e7d4eeec5cdf06f6
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-13 14:58:22 +0530

    ✅ PYTH66 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-13

--- codechef/medium/PYTH66/solution.py
+++ codechef/medium/PYTH66/solution.py
@@ -4,4 +4,6 @@ r = int(input())
 if r>b:
     print("Rob scored higher marks than Bob")
 elif r==b:
-    print("Bob & Rob both scored the same")
+    print("Bob & Rob scored the same")
+else:
+    print("Bob scored higher marks than Rob")
