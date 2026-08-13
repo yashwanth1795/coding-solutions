07b45b56e959f541cf1d63545163f6e3daf6bd39
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-13 14:57:11 +0530

    ✅ PYTH73 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/PYTH73/solution.py
@@ -0,0 +1,10 @@
+# Update the blanks in the code below to solve the problem
+
+r, k = map(int, input().split())
+
+if r>k:
+    print("Ram is heavier than Karan")
+elif r<k:
+    print("Karan is heavier than Ram")
+else:
+    print("Ram & Karan have the same weight")
