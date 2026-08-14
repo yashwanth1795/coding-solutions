5290b44b340d822075d633a77ebf00227231113d
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-14 19:37:29 +0530

    ✅ PYTH76 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/PYTH76/solution.py
@@ -0,0 +1,9 @@
+# Update the code below to solve the problem
+
+a = 15
+
+# a%7 returns the remainder when a is divided by 7
+if (a%7 == 0) and (a%5 == 0):       
+    print("The number is divisble both 5 & 7")
+else:
+    print("The number is not divisible by both 5 & 7")
