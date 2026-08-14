1c5f3fcd67a3078357b8f425b3d3e8c0a1ed6c24
yashwanth1795 <gowniyashwanth72@gmail.com>
2026-08-14 19:52:01 +0530

    ✅ DEBUGPYL8 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-14

--- codechef/medium/DEBUGPYL8/solution.py
+++ codechef/medium/DEBUGPYL8/solution.py
@@ -1,7 +1,7 @@
 #if condition is wrong it should be changed to n>5
 n = int(input())
 
-if n < 5:
+if n >5:
     print("the number is greater than 5")
 else:
     print("the number is smaller than or equal to 5")
